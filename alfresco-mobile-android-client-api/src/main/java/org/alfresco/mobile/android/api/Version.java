82629233af7001cadd345414f47b2cd6810becdf
jm.pascal <jeanmarie.pascal@alfresco.com>
2013-01-19 22:21:49 +0100

    MOBSDK-455 : Version Number.

---
+++ alfresco-mobile-android-client-api/src/main/java/org/alfresco/mobile/android/api/Version.java
@@ -0,0 +1,10 @@
+package org.alfresco.mobile.android.api;
+
+public final class Version
+{
+    //Current Version of SDK
+    static final String SDK = "1.1.0";
+    
+    //Current Version of SDK
+    static final int SDK_INT = 1100;
+}
